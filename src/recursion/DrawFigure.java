cc4a9e342f02db6370c93ae36807124c986d9796
lozevBogdan <lozev.bogdan@abv.bg>
2022-08-18 16:01:12 +0300

    Recursion for drawing mirror figure.

---
+++ src/recursion/DrawFigure.java
@@ -0,0 +1,36 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class DrawFigure {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number, please:");
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        drawPicture(num);
+
+    }
+
+    public static void drawPicture(int n){
+        if( n == 0) {
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        drawPicture(n - 1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("#");
+        }
+        System.out.println();
+    }
+
+}
